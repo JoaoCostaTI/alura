d564f4079968b51388427888fbad952de5c09ecd
JoaoCostaTI <joao.costapp@outlook.com>
2025-09-30 14:41:31 -0300

    as

--- app.js
+++ app.js
@@ -1,9 +1,18 @@
-alert('Boas vindas ao nosso site! ')
-let nome = "Lua"
-let idade = 25
-let numeroDeVendas = 50
-let saldoDisponivel = 1000
+alert('Bem vindo ao jogo do número secreto! ')
+let numeroSecreto = 2;
+console.log(numeroSecreto);
+let chute, tentativas = 1;
 
-let mensagemDeErro = 'Erro! Preencha todos os campos!'
-alert(mensagemDeErro)
-
+while (chute != numeroSecreto){
+    chute = prompt('Escolha um numero entre 1 e 10: ')
+    if(chute == numeroSecreto){
+        alert(`Isso ai! Você descobriu o numero secreto que é: ${numeroSecreto}\nE acertou com ${tentativas} tentativas`);
+    }else{
+        if(chute > numeroSecreto){
+            alert(`O numero secreto é menor que ${chute}\nNumero de tentativas: ${tentativas}`);
+        }else {
+            alert(`O numero secreto é maior que ${chute}\nNumero de tentativas: ${tentativas}`);
+        }
+        tentativas++;
+    }
+}
---
+++ testes/app.js
@@ -0,0 +1,7 @@
+let diaDaSemana = prompt('Qual é o dia da semana? ')
+
+if(diaDaSemana == 'Sabado' | diaDaSemana == 'Domingo'){
+    alert('Um excelente final de semana! ')
+}else{
+    alert(`Boa semana! Hoje é: ${diaDaSemana}`)
+}
