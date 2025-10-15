8d217055d5d0164eb72cd53377d863c853e3c436
JoaoCostaTI <joao.costapp@outlook.com>
2025-10-15 15:15:07 -0300

    a

--- arena/app.js
+++ arena/app.js
@@ -1,24 +1,4 @@
-/*
-10 itens ou mais, e/ou valor unitário igual ou superior a 100 reais
-*/ 
 
-function calcular(){
-    /*
-    let quantidade = parseInt(document.getElementById('quantidade').value);
-    let valor = parseFloat(document.getElementById('valor').value);
-    */
-    let quantidade = 12
-    let valor = 120
-    let total = quantidade * valor;
-
-    if(quantidade >= 10 || valor >= 100){
-        total = total - (total/100 * 5)
-    }
-
-    console.log(`Valor Total: R$${total}`)
-}
-
-function reiniciar(){
-    quantidade.value = ''
-    valor.value = ''
+function clicou(){
+    window.confirm('Tem certeza? ')
 }
