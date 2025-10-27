3921e4ba40f241f4f841d67aa4aae4cd75b1d602
JoaoCostaTI <joao.costapp@outlook.com>
2025-10-27 06:27:17 -0300

    as

--- cursoLogicaDeProgramacao/amigo-secreto/js/app.js
+++ cursoLogicaDeProgramacao/amigo-secreto/js/app.js
@@ -23,7 +23,7 @@ function adicionar() {
     amigo.value = '';
 }
 
-<<<<<<< HEAD
+
 function sortear(){
     embaralha(listaCompletaAmigos)
     let LA = document.getElementById('lista-sorteio')
@@ -36,43 +36,4 @@ function sortear(){
         }
     }
 }
-=======
-function sortear() {
-    if(amigos.length < 4){
-        return alert('Adicione pelo menos 4 amigos.')
-    }
-    embaralhar(amigos);
->>>>>>> 16a7757d9e7264aba14646c5cbc32297298c40fc
 
-    let sorteio = document.getElementById('lista-sorteio');
-    for (let i = 0; i < amigos.length; i++) {
-        if (i == amigos.length - 1) {
-            sorteio.innerHTML = sorteio.innerHTML + amigos[i] +' --> ' +amigos[0] + '<br/>';
-        } else {
-            sorteio.innerHTML = sorteio.innerHTML + amigos[i] +' --> ' +amigos[i + 1] + '<br/>';
-        }
-    }
-}
-
-<<<<<<< HEAD
-function reiniciar(){
-    document.getElementById('nome-amigo').value = ""
-    listaAmigos = document.getElementById('lista-amigos')
-    listaAmigos.textContent = ""
-    let limparLA = document.getElementById('lista-sorteio')
-    limparLA.textContent = ""
-    listaCompletaAmigos = []
-=======
-function embaralhar(lista) {
-    for (let indice = lista.length; indice; indice--) {
-        const indiceAleatorio = Math.floor(Math.random() * indice);
-        [lista[indice - 1], lista[indiceAleatorio]] = [lista[indiceAleatorio], lista[indice - 1]];
-    }
->>>>>>> 16a7757d9e7264aba14646c5cbc32297298c40fc
-}
-
-function reiniciar() {
-    amigos = [];
-    document.getElementById('lista-amigos').innerHTML = '';
-    document.getElementById('lista-sorteio').innerHTML = '';
-}
